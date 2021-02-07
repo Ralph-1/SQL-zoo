fdcb3f01bdb16b5e473db1d99957f0d3340ccb20
Ralph <ralphoburu@gmail.com>
2021-02-07 10:34:06 +0300

    add sql solution

--- new.sql
+++ new.sql
@@ -115,21 +115,55 @@ WHERE name='Russia');
 
 SELECT name FROM world
 WHERE continent = 'Europe' 
-AND GDP/population > (SELECT GDP/population FROM world WHERE name = 'United Kingdom')
+AND GDP/population > (SELECT GDP/population FROM world WHERE name = 'United Kingdom');
 
 SELECT name, continent FROM world
 WHERE continent IN (SELECT continent FROM world WHERE name = 'Argentina' OR name = 'Australia')
-ORDER BY name
+ORDER BY name;
 
 SELECT name, population FROM world
 WHERE population > (SELECT population FROM world WHERE name = 'Canada') 
-AND population < (SELECT population FROM world WHERE name = 'Poland')
+AND population < (SELECT population FROM world WHERE name = 'Poland');
 
 SELECT name, CONCAT(ROUND(population/(SELECT population FROM world WHERE name = 'Germany') * 100, 0), '%') 
 FROM world
-WHERE continent = 'Europe'
+WHERE continent = 'Europe';
 
 SELECT SUM(population)
-FROM world
+FROM world;
 
-SELECT DISTINCT continent FROM world
+SELECT DISTINCT continent FROM world;
+
+SELECT SUM(GDP) FROM world
+WHERE continent = 'Africa';
+
+SELECT COUNT(name) FROM world
+WHERE area >= 1000000;
+
+SELECT SUM(population) FROM world
+WHERE name 
+IN ('Estonia', 'Latvia', 'Lithuania');
+
+SELECT continent, COUNT(name) FROM world
+GROUP BY continent;
+
+SELECT continent, COUNT(name) FROM world
+WHERE population >= 10000000
+GROUP BY continent;
+
+SELECT continent
+FROM world
+GROUP BY continent
+HAVING SUM(population) >= 100000000;
+
+SELECT player, teamid, stadium, mdate
+FROM game JOIN goal ON (id=matchid)
+WHERE teamid = 'GER'
+
+SELECT team1, team2, player FROM game JOIN goal ON (id = matchid)
+WHERE player LIKE 'Mario%';
+
+SELECT player, teamid, coach, gtime
+FROM goal JOIN eteam ON (teamid = id)
+WHERE gtime<=10;
+
